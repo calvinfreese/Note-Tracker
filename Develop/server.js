3f149c5d42eca9486e2040d47b18a8bb20792a67
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-09-22 19:44:15 -0500

    added missing paran

--- Develop/server.js
+++ Develop/server.js
@@ -16,7 +16,7 @@ app.get("*", function(req, res){
 });
 
 app.get("/notes", function(req,res){
-    res.sendFile(path.join(publicFolder, "notes.html");
+    res.sendFile(path.join(publicFolder, "notes.html"));
 });
 
 
