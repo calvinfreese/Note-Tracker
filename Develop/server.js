d73f1a99a3a4525698dc9e5ec1b036cfa9451064
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-09-22 20:15:04 -0500

    added app.get for api/notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var path = require('path');
+const fs = require('fs');
 var app = express();
 var PORT = process.env.PORT || 3000;
+
 var publicFolder = path.resolve(__dirname, "public");
+var dbFolder = path.resolve(__dirname, "db");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -23,7 +26,12 @@ app.get("/notes", function(req,res){
     res.sendFile(path.join(publicFolder, "notes.html"));
 });
 
-
+app.get("/api/notes", function(req, res){
+    fs.readFile(path.join(dbFolder, "db.json"), function(err, data){
+        return res.json(data);
+    });
+    
+})
 
 
 //Do not delete listener
