c54971bdcd6da6fbc2dbc562d5a8678ed9a5428e
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-09-22 19:33:31 -0500

    added in express, PORT listener, and app.get for index.html (*)

---
+++ Develop/server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var publicFolder = path.resolve(__dirname, "public");
+// Add routing below
+
+app.get("*", function(req, res){
+    console.log(publicFolder);
+    res.sendFile(path.join(publicFolder, "index.html"));
+});
+
+
+
+
+
+//Do not delete listener
+app.listen(PORT, function(){
+    console.log("App is listening on PORT " + PORT);
+})
