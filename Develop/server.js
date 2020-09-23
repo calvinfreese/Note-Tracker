679a2645ba4144ff9851ef16395b16042d92bb51
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-09-22 20:01:10 -0500

    added in express.static to fix MIME-type error thrown when trying to load css on page load of notes.html

--- Develop/server.js
+++ Develop/server.js
@@ -1,21 +1,25 @@
 var express = require('express');
 var path = require('path');
-
 var app = express();
 var PORT = process.env.PORT || 3000;
+var publicFolder = path.resolve(__dirname, "public");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var publicFolder = path.resolve(__dirname, "public");
+//without this, a MIME-type error is thrown.
+app.use(express.static(__dirname + '/public'));
+
+
 // Add routing below
 
-app.get("*", function(req, res){
-    console.log(publicFolder);
+app.get("/", function(req, res){
+    console.log('index');
     res.sendFile(path.join(publicFolder, "index.html"));
 });
 
 app.get("/notes", function(req,res){
+    console.log('notes');
     res.sendFile(path.join(publicFolder, "notes.html"));
 });
 
