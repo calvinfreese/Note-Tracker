1b6a970bde5020f349b5f01163df1d78336843a2
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-09-24 22:58:51 -0500

    added delete function. added read/write ability to json file

--- Develop/routes/apiRouting.js
+++ Develop/routes/apiRouting.js
@@ -1,6 +1,7 @@
 //require in database file
 var notes = require('../db/db');
 var uniqid = require('uniqid');
+const fs = require('fs');
 
 //export to server.js
 module.exports = function(app){
@@ -11,11 +12,11 @@ module.exports = function(app){
     app.get('/api/notes/:note', function(req, res){
         var selectedNote = req.params.note;
 
-        console.log(selectedNote);
+        
 
         for (var i = 0; i < notes.length; i++) {
             if (selectedNote === notes[i].id) {
-            //   notes.splice(i, 1);
+            console.log(selectedNote);
               return res.json(notes[i])
             }
           }
@@ -28,10 +29,37 @@ module.exports = function(app){
         newNote.id = uniqid.time();
 
         notes.push(newNote);
-
+        console.log(newNote);
         res.json(newNote);
+
+        fs.readFile('./db/db.json', function(err, data){
+            if (err) throw err;
+            let jsonArr = JSON.parse(data);
+
+            jsonArr.push(newNote);
+            
+            fs.writeFile('./db/db.json', JSON.stringify(jsonArr), function(err){
+                if (err) throw err;
+                console.log('stored data to db');
+            })
+
+        });
+        
     });
 
-    
+    app.delete('/api/notes/:noteID', function(req, res){
+        var selectedNote = req.params.noteID;
 
+        for (var i = 0; i < notes.length; i++) {
+            if (selectedNote === notes[i].id) {
+            notes.splice(i,1);
+            return res.json(notes)
+            }
+        }
+        
+    });
+//end of function
 };
+
+
+
